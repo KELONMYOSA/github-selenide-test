b81d99f97b6a022a9306f386a975e7ddc79c5ebb
KELONMYOSA <79825223592@yandex.ru>
2022-02-23 21:10:56 +0300

    the first test is done

--- src/test/java/tests/GithubSelenideTest.java
+++ src/test/java/tests/GithubSelenideTest.java
@@ -1,4 +1,28 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class GithubSelenideTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void FindingSoftAssertionsTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-box").$(withText("more pages")).click();
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").$(byText("Using JUnit5 extend test class:")).parent().sibling(0).shouldNotBe(empty);
+    }
 }
